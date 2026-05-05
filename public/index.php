5a09209dc6f4cb13b057a42039d7ba842e97e211
anch365 <mchindaanchoura@gmail.com>
2026-05-05 09:16:51 +0200

    Add titles of exercices

--- public/index.php
+++ public/index.php
@@ -4,15 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../assets/styles/style.css">
 </head>
 <body>
+    <h2>Exercice 1</h2>
     <?php 
-    if (isset($_GET['lastname'])&&isset($_GET['firstname'])) { ?>
+    if (isset($_GET['lastname']) && isset($_GET['firstname'])) { ?>
        <p>Nom : <?= htmlspecialchars($_GET['lastname']) ?><br> 
        Prenom : <?= htmlspecialchars($_GET['firstname']) ?></p> 
     <?php } else { ?>
-        <p>Les paramètres sont manquants</p>
+        <p class="error">Les paramètres sont manquants</p>
     <?php }
     ?>
+    <h2>Exercice 2</h2>
+     <?php 
+    if (isset($_GET['lastname']) && isset($_GET['firstname'])) { ?>
+       <p>Nom : <?= htmlspecialchars($_GET['lastname']) ?><br> 
+       Prenom : <?= htmlspecialchars($_GET['firstname']) ?></p> 
+    <?php } ?>
 </body>
 </html>
